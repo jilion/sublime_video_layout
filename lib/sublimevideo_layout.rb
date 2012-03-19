c0bd3df00511b42709709ab8a2b1e2ecbbdf31b0
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2012-03-19 14:06:11 +0100

    Add public folder

--- lib/sublimevideo_layout.rb
+++ lib/sublimevideo_layout.rb
@@ -6,5 +6,8 @@ module SublimevideoLayout
     config.to_prepare do
       # ApplicationController.layout 'sublimevideo'
     end
+    initializer "static assets" do |app|
+      app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
+    end
   end
 end
