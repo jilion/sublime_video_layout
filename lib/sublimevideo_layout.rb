c65cccdd34d6d0c48c090d50883bb06ed7aa9ab4
Rémy Coutable <remy@jilion.com>
2012-04-13 12:22:20 +0200

    I don't think we need to insert ActionDispatch::Static 2 times (might fix an issue)! :)

--- lib/sublimevideo_layout.rb
+++ lib/sublimevideo_layout.rb
@@ -8,9 +8,6 @@ module SublimevideoLayout
     initializer 'assets' do |app|
       app.config.assets.precompile += %w[sublimevideo.css sublimevideo.js ie.css]
     end
-    initializer 'static assets' do |app|
-      app.middleware.insert_after Rack::Lock, ::ActionDispatch::Static, "#{root}/public"
-    end
     initializer 'helpers' do |app|
       ActionView::Base.send :include, SublimeVideoLayoutHelper
     end
