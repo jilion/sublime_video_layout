1fbf83c565975b92fe278118e6194820dbf93b1f
Rémy Coutable <remy@jilion.com>
2012-08-17 19:46:17 +0200

    Compile errors.css!

--- lib/sublimevideo_layout.rb
+++ lib/sublimevideo_layout.rb
@@ -5,7 +5,7 @@ require 'haml'
 module SublimevideoLayout
   class Engine < ::Rails::Engine
     initializer 'assets' do |app|
-      app.config.assets.precompile += %w[sublimevideo.css sublimevideo.js ie.css]
+      app.config.assets.precompile += %w[errors.css ie.css sublimevideo.css sublimevideo.js]
     end
     initializer 'static assets' do |app|
       app.middleware.insert_before Rack::Lock, ::ActionDispatch::Static, "#{root}/public"
