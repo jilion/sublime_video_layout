5779d1c001a2dcbdb0c06a91afb47c6f6a52fa58
Rémy Coutable <remy@jilion.com>
2012-09-27 12:21:30 +0200

    Remove legacy code

--- spec/internal/config/routes.rb
+++ spec/internal/config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
   root to: 'application#page', page: 'home'
-  get 'error_404', to: 'errors#error_404'
-  get 'error_500', to: 'errors#error_500'
   get ':page' => 'application#page'
 
 end
