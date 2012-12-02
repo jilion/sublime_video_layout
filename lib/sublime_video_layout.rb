b7077c9934391e8b99e889940d1cd9182fb67d9e
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2012-12-02 13:01:10 +0100

    Remove sublimevideo.js|css from assets.precompile

--- lib/sublime_video_layout.rb
+++ lib/sublime_video_layout.rb
@@ -5,7 +5,7 @@ require 'haml'
 module SublimeVideoLayout
   class Engine < ::Rails::Engine
     initializer 'assets' do |app|
-      app.config.assets.precompile += %w[errors.css ie.css sublimevideo.css sublimevideo.js]
+      app.config.assets.precompile += %w[errors.css ie.css]
     end
     initializer 'static assets' do |app|
       app.middleware.insert_before Rack::Lock, ::ActionDispatch::Static, "#{root}/public"
