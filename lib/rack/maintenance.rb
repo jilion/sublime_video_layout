afa6a4f51912164502db46f67b86eb1e1eb37031
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2012-12-13 22:01:20 +0100

    Revert "Allow to pass a POST param to bypasse the fake maintenance"
    This reverts commit 2c7085fac8fcfa6d908a20a27301cddb042c217d.

--- lib/rack/maintenance.rb
+++ lib/rack/maintenance.rb
@@ -21,8 +21,7 @@ module Rack
         [302, headers.merge('Location' => '/'), ['Redirected to /']]
       else
         request = ActionDispatch::Request.new(env)
-        param_or_cookie = request.cookie_jar[:maintenance] || request.POST['maintenance_code']
-        if param_or_cookie == ENV['MAINTENANCE_CODE']
+        if request.cookie_jar[:maintenance] == ENV['MAINTENANCE_CODE']
           @app.call(env)
         else
           [302, {'Location' => '/maintenance'}, ['Redirected to /maintenance']]
