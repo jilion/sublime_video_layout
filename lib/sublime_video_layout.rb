8efc1022beed3f6f1373190312390fa15f83400f
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2013-01-23 09:20:20 +0100

    Add not about assets.precompile & initialize_on_precompile

--- lib/sublime_video_layout.rb
+++ lib/sublime_video_layout.rb
@@ -5,6 +5,7 @@ require 'haml'
 module SublimeVideoLayout
   class Engine < ::Rails::Engine
     initializer 'assets' do |app|
+      # Should be duplicated to application when initialize_on_precompile is false
       app.config.assets.precompile += %w[errors.css ie.css]
     end
     initializer 'static assets' do |app|
